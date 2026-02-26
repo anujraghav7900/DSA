e3213456de8a758cf4f3b6da39bdd4eb676722f5
Anuj Raghav <149610752+anujraghav7900@users.noreply.github.com>
2026-02-26 09:45:44 +0530

    Time: 78 ms (81.74%) | Memory: 13.5 MB (81.41%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int total = 0;
+        for(int num : nums)
+            total += num;
+        
+        // If total sum is odd, partition not possible
+        if(total % 2 != 0)
+            return false;
+        
+        int target = total / 2;
+        
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;  // base case
+        
+        for(int num : nums) {
+            for(int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
