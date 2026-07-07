44ed62f28d421c0ecf6e930a136089585e2a61d3
Anuj Raghav <149610752+anujraghav7900@users.noreply.github.com>
2026-07-07 11:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (63.96%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -5,13 +5,12 @@ public:
         int count = 0;
         ListNode* temp = head;
 
-        // Count nodes
         while (temp != NULL) {
             count++;
             temp = temp->next;
         }
 
-        // Move to middle
+        
         temp = head;
         for (int i = 0; i < count / 2; i++) {
             temp = temp->next;
